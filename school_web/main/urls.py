e3b606309552521894bf6aff83c272e6aea5d918
Mishel <mihailgavrik03@gmail.com>
2023-07-06 15:31:53 +0300

    Добавлена страница "Об онлайн школе" На страницк "об оналайн школе" добавлен переход на "курсы" Переименовал название ссылок на главной странице

--- school_web/main/urls.py
+++ school_web/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('online-school', views.online_info, name='online_info')
 ]
