06a4f7ab99f8dfe84b3d00b65833a1e1c4c7079d
Mishel <mihailgavrik03@gmail.com>
2023-07-09 20:47:47 +0300

    добавил файл миграций, создал таблицу в базе данных проверка работы сохранения

--- school_web/courses/models.py
+++ school_web/courses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Queries(models.Model):
+    fio = models.CharField('ФИО', max_length=50)
+    phone_number = models.CharField('Номер телефона', max_length=13)
+    type_of_learning = models.BooleanField(default=True)
+    sr_mark = models.FloatField('Средний балл')
+
+    def __str__(self):
+        return self.fio
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
