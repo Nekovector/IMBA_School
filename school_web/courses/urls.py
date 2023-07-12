505e0b4ab37b0ce03548ee64ddd57cf2d7114519
Mishel <mihailgavrik03@gmail.com>
2023-07-12 18:32:55 +0300

    Не рабочий вариант! не мержить с мастером Делается работа кнопками улучешния успеваемости исправил drade на grade

--- school_web/courses/urls.py
+++ school_web/courses/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.directions, name='courses_home'),
     path('entry-form', views.entryForm, name='passport'),
-    path('improving_academic_performance', views.grade_info, name='drade_info')
+    path('improving_academic_performance', views.grade_info, name='grade_info')
 ]
