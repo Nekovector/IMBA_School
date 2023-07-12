88f78b576388f0ae981ccdd8df6cf74274b1e39c
Иван <ivanmatuyhin@gmail.com>
2023-07-12 16:35:08 +0300

    Реализована обработка 2-х форм (Иоанн гений)

--- school_web/courses/admin.py
+++ school_web/courses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Queries
+from .models import Queries1, Queries2
 
-admin.site.register(Queries)
+admin.site.register(Queries1)
+admin.site.register(Queries2)
--- school_web/courses/views.py
+++ school_web/courses/views.py
@@ -1,20 +1,51 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import QueriesForm
+from .forms import Queries1Form, Queries2Form
+
 
 def directions(request):
     return render(request, 'courses/directions.html')
 
 
+class Queries3Form:
+    pass
+
+
 def entryForm(request):
-    if request.method == 'POST':
-        form = QueriesForm(request.POST)
-        if form.is_valid():
-            form.save()
+    category_id = request.GET.__getitem__('category')
+    if request.method == 'GET':
+        if category_id == '1':
+            form = Queries1Form()
+            data = {
+                'form': form
+            }
+            return render(request, 'courses/entryForm1.html', data)
+        elif category_id == '2':
+            form = Queries2Form()
+            data = {
+                'form': form
+            }
+            return render(request, 'courses/entryForm2.html', data)
+        elif category_id == '3':
+            form = Queries3Form()
+            data = {
+                'form': form
+            }
+            return render(request, 'courses/entryForm3.html', data)
+        else:
             return redirect('home')
-    form = QueriesForm()
 
-    data = {
-        'form': form
-    }
-
-    return render(request, 'courses/entryForm.html', data)
+    if request.method == 'POST':
+        if category_id == '1':
+            form = Queries1Form(request.POST)
+            if form.is_valid():
+                form.save()
+        if category_id == '2':
+            form = Queries2Form(request.POST)
+            if form.is_valid():
+                form.save()
+        if category_id == '3':
+            form = Queries3Form(request.POST)
+            if form.is_valid():
+                form.save()
+        return redirect('home')
